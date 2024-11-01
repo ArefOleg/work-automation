0eb84aaa55b514060f2e68af6d097d19df74ea83
ArefOleg <serd-075@mail.ru>
2024-11-01 16:11:39 +0300

    update

--- Pages/EAI.cshtml.cs
+++ Pages/EAI.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Work_Menu;
@@ -7,7 +9,7 @@ namespace work_automation.Pages;
 
 public class EaiModel : PageModel
 {   public Menu mainMenu;
-    public List<LineItems> lineItems = new List<LineItems>();
+    public List<LineItems> lineItems;
     private readonly ILogger<IndexModel> _logger;
     public string Message { get; private set; } = "";
     public EaiModel(ILogger<IndexModel> logger)
@@ -20,16 +22,35 @@ public class EaiModel : PageModel
     {        
         Message = "OnGet";
     }
-    public void OnPost(string? PumpNum, string LineNumber, string Product, string NetPrice,
-    string QuantityRequested)    
-    {
+    public void OnPost(string? PumpNum, string? LineNumber, string? Product, string? NetPrice,
+    string? QuantityRequested)    
+    {//нужно записывать json с продуктами в файл
+    //после заполнения чека, формировать итоговое сообщение
         Message = "OnPost";
-        if(PumpNum is not null){
-            LineItems lineItemsOne = new LineItems(PumpNum, LineNumber, Product,
-            NetPrice, QuantityRequested);
-            lineItems.Add(lineItemsOne);
+    /*    if(PumpNum is not null){
+            using (FileStream fs = new FileStream("wwwroot/sources/menu/line item.json",
+             FileMode.OpenOrCreate)){
+                LineItems lineItemsOne;
+                try{
+                    lineItems = new List<LineItems>();                
+                    lineItems = await JsonSerializer.DeserializeAsync<List<LineItems>>(fs);
+                    byte[] buffer = Encoding.Default.GetBytes("");
+                    await fs.WriteAsync(buffer, 0, buffer.Length);
+                }
+                catch{
+                    lineItemsOne = new LineItems(PumpNum, LineNumber, Product,
+                    NetPrice, QuantityRequested);
+                    lineItems.Add(lineItemsOne);
+                    await JsonSerializer.SerializeAsync<List<LineItems>>(fs, lineItems); 
+                }
+                    lineItemsOne = new LineItems(PumpNum, LineNumber, Product,
+                    NetPrice, QuantityRequested);
+                    lineItems.Add(lineItemsOne);
+                    await JsonSerializer.SerializeAsync<List<LineItems>>(fs, lineItems); 
+                                
+             }            
         }
-        
+        */
 
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,18 +6,15 @@ namespace work_automation.Pages;
 
 public class IndexModel : PageModel
 {   public Menu mainMenu;
-    private readonly ILogger<IndexModel> _logger;
+    
 
     public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-        mainMenu = new Menu();
+    {        
+        
     }
 
-    public void OnGet(string? menu)
-    {
-        
-        
-        if(menu == "EAI"){mainMenu.eaiMenu();}
+    public void OnGet()
+    {       
+        mainMenu = new Menu();
     }
 }
