fdede68d30834199ced13409624a72ae2e4fcbea
ArefOleg <serd-075@mail.ru>
2024-11-01 13:38:35 +0300

    update

--- Pages/EAI.cshtml.cs
+++ Pages/EAI.cshtml.cs
@@ -7,23 +7,23 @@ namespace work_automation.Pages;
 
 public class EaiModel : PageModel
 {   public Menu mainMenu;
-    public List<LineItems> lineItems;
+    public List<LineItems> lineItems = new List<LineItems>();
     private readonly ILogger<IndexModel> _logger;
     public string Message { get; private set; } = "";
     public EaiModel(ILogger<IndexModel> logger)
     {
         Message = "Введите свое имя";
-        lineItems = new List<LineItems>();
+        
     }
 
     public void OnGet()
-    {
-        
-        Message = "Введите свое имя";
+    {        
+        Message = "OnGet";
     }
     public void OnPost(string? PumpNum, string LineNumber, string Product, string NetPrice,
-    string QuantityRequested)
+    string QuantityRequested)    
     {
+        Message = "OnPost";
         if(PumpNum is not null){
             LineItems lineItemsOne = new LineItems(PumpNum, LineNumber, Product,
             NetPrice, QuantityRequested);
