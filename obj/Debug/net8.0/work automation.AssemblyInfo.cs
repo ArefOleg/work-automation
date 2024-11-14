11310f170ac4f6c2d5bd84c6018b06226a89733a
ArefOleg <serd-075@mail.ru>
2024-11-14 14:47:55 +0300

    update

--- obj/Debug/net8.0/work automation.AssemblyInfo.cs
+++ obj/Debug/net8.0/work automation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("work automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063af1cddabba3be68d82c945bb1fb969bd3b681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f74c667a2ffa22abf203ace302510250238fff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("work automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("work automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
