13d6aedfeb00b66a0986d2b7088195de1d644d5d
ArefOleg <serd-075@mail.ru>
2024-12-01 22:06:34 +0300

    update

---
+++ Model/Task.cs
@@ -0,0 +1,11 @@
+public class Task{
+    public string name{get; set;}
+    public string file{get; set;}
+
+}
+
+public class Task_Object{
+    public string name{get; set;}
+    public string type{get; set;}
+    public string about{get; set;}
+}
