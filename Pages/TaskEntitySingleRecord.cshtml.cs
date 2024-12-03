786210c152238d404a5fde494b8992e339e48da6
ArefOleg <serd-075@mail.ru>
2024-12-03 14:54:01 +0300

    update

--- Pages/TaskEntitySingleRecord.cshtml.cs
+++ Pages/TaskEntitySingleRecord.cshtml.cs
@@ -8,9 +8,10 @@ namespace work_automation.Pages;
 public class TaskEntitySingleRecordModel : PageModel
 {   public TaskEntity taskEntity{get; set;}
     
-    public void OnGet()
+    public void OnGet(int id)
     {
-                     
+        TaskEntityController taskEntityController = new TaskEntityController();
+        taskEntity = taskEntityController.getTaskEntityById(id);
     }
     
 }
