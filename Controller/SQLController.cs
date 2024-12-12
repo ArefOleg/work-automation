0c17b450de3ed25030b82fe8665d86627ef2c730
ArefOleg <serd-075@mail.ru>
2024-12-12 07:24:27 +0300

    update

---
+++ Controller/SQLController.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using SQLSpace;
+
+public class SQLController{
+    public void createSQLRec(string name, string about, string sqlBody){
+        using (ApplicationContext db = new ApplicationContext()){            
+            SQLEntity sQLEntity = new SQLEntity
+             { name = name, about = about, sqlBody = sqlBody, created = DateTime.Now};
+            db.taskEntities.AddRange(taskEntity);
+            db.SaveChanges();
+        }
+    }
+    public void deleteSQLRec(int Id){
+        using(ApplicationContext db = new ApplicationContext()){
+            db.sqlEntities.Where(p => p.Id == Id).ExecuteDelete();
+        }
+    }
+
+    public List<SQLEntity> getSQLRecs(){
+        var entities = new List<SQLEntity>();
+        using(ApplicationContext db = new ApplicationContext()){
+            entities = (from entity in db.sqlEntities select entity).ToList();            
+        }
+        entities = entities.OrderByDescending(e=>e.created).ToList();
+        return entities;
+    }
+
+    public SQLEntity getSQLEntity(){
+        SQLEntity sQLEntity;
+        using(ApplicationContext db = new ApplicationContext()){
+            sQLEntity = db.sqlEntities.Find(Id);  
+        }
+        return sQLEntity;
+    }
+}
--- Model/SQLEntity.cs
+++ Model/SQLEntity.cs
@@ -1,5 +1,9 @@
+namespace SQLSpace;
 public class SQLEntity{
     public int Id{get; set;}
+    public string name{get; set;}
+    public string about{get; set;}
     public string sqlBody{get; set;}
-    
+    public DateTime created{get; set;}
+
 }
