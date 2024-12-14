51cab043714aa6c028b7bc4dedfbae97a21c3536
ArefOleg <serd-075@mail.ru>
2024-12-14 09:13:28 +0300

    udpate

---
+++ Pages/SQLTask/SQLTaskAll.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Work_Menu;
+using TaskEntitys;
+using SQLEntity;
+
+namespace work_automation.Pages;
+
+public class SQLTaskAllModel : PageModel
+{   public List<SQLEntity> sqlEntities{get; set;}
+    
+    public void OnGet(int? deleteId)
+    {
+        SQLController sqlController = new SQLController();
+        if(deleteId != null){
+            sqlController.deleteSQLRec((int)deleteId);
+        }
+        
+        sqlEntities = SQLController.getSQLRecs();        
+    }
+    
+}
